2d653161fd5cb1f90fea997c06de752e11ef2670
Ricardo2099 <ricardorojas104@gmail.com>
2025-03-12 10:26:06 -0600

    work-experiecience

--- src/app/services/header-service/header.service.ts
+++ src/app/services/header-service/header.service.ts
@@ -4,6 +4,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class HeaderService {
+  accesoHeader: string = 'Valor inicial desde HeaderService';
 
-  constructor() { }
+  constructor() {
+    console.log('HeaderService constructor');
+  }
 }
