713c6fc79250d81fb1dfd57527654c82dfd5de74
Ricardo2099 <ricardorojas104@gmail.com>
2025-04-06 22:21:02 -0600

    arreglos

--- src/app/services/work-experience-service/work-experience.service.ts
+++ src/app/services/work-experience-service/work-experience.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+import { WorkExperience } from '../../models/work-experience/work-experience.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorkExperienceService {
+  constructor(private firestore: AngularFirestore) {}
 
-  constructor() { }
+  getWorkExperiences(): Observable<WorkExperience[]> {
+    return this.firestore.collection<WorkExperience>('work-experience').valueChanges({ idField: 'id' });
+  }
 }
